c9f55780a5fcd7531a8af8303db9be55932b7889
Madhav Poudel <madhavpoudel78@nepathyacollege.edu.np>
2025-11-20 12:57:32 +0545

    add function implemented

--- index.js
+++ index.js
@@ -1,1 +1,93 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+
+
+// Variables in JavaScript
+// let program = "HELLO";
+// console.log(program);
+
+// Constants in JavaScript
+// const pi = 3.14;
+// console.log(pi);
+
+// Functions in JavaScript
+
+
+
+
+
+
+
+// Guess The Output
+// const x = 5;
+// let y = 10;
+
+// y = y + x;
+
+// console.log("Final value of y is:", y);
+
+
+
+
+
+
+// Guess The Output
+
+// let x = 10;
+
+// if (true) {
+//     let x = 20;
+//     console.log("Inside:", x);
+// }
+// console.log("Outside:", x);
+
+
+
+// Guess The Output
+// {
+//     let y = 50;
+// }
+
+// console.log(y);
+
+
+// Guess The Output
+// let score = 88;
+// let extraCredit = 5;
+
+// if(score + extraCredit >= 95){
+//     console.log("Grade: A+");
+// }else if(score >= 85 && score < 95){
+//     console.log("Grade: A");
+// }else if(score >= 75 || extraCredit > 0){
+//     console.log("Grade: B");
+// }else{
+//     console.log("Grade: C");
+// }
+
+
+
+// Function In JavaScript
+
+// A function is a reusable block of code that performs a specific task. 
+// It helps avoid repetition and makes code organized.
+
+
+// Example:
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(5, 3));
+
+
+
+// Create a function named doubleNumber that:
+
+// Takes one number
+
+// Returns the number multiplied by 2
+
+// Print the result
+
+
