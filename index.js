62d324d36ec8a1aac94d2d24ccc4c5f45b87b3f1
Madhav Poudel <madhavpoudel78@nepathyacollege.edu.np>
2025-11-24 14:17:02 +0545

    Modules in JS

--- index.js
+++ index.js
@@ -456,3 +456,91 @@
 // }
 
 // test();
+
+
+
+
+// Task:
+// Create an async function greetUser.
+// Inside it, print "Hello" first.
+// Then await a promise that resolves immediately with "Welcome!".
+// Finally, print the resolved message.
+
+
+
+// What is Error Handling?
+// Error handling is how a program deals with mistakes or problems that happen while
+//  it’s running. Instead of crashing, the program can catch the error and respond 
+// safely.
+
+
+// 1. Try-Catch
+// try → code that might cause an error goes here.
+// catch → what to do if an error happens.
+
+
+// 2. Finally
+// finally always runs, no matter if there’s an error or not.
+// Useful for cleanup (closing files, stopping timers).
+
+
+
+// 3. Throwing Errors
+// You can create your own error when something is wrong using throw.
+
+
+// 4. Promise Error Handling
+// For asynchronous code:
+// .catch() handles promise errors
+// With async-await, use try-catch.
+
+
+// 5. Error Types
+// Common JavaScript errors:
+// Error → general error
+// TypeError → wrong type (like number + string)
+// ReferenceError → variable doesn’t exist
+
+function divide(a, b) {
+    try {
+        if (b === 0) throw "Cannot divide by zero!"; // Throw error if b is 0
+        let result = a / b;
+        console.log("Result:", result);
+    } catch (error) {
+        console.log("Error:", error); // Handle the error
+    } finally {
+        console.log("Division attempt finished."); // Always runs
+    }
+}
+
+// Test cases
+divide(10, 2); // Works fine
+divide(10, 0); // Error handled
+
+
+// Task:
+// Create a function checkNumber(num) that:
+// Throws an error "Number must be positive!" if num < 0.
+// Otherwise, returns the number.
+// Create another function getData(num) that:
+// Uses try-catch-finally.
+// Calls checkNumber(num) and prints "Valid number: " if no error.
+// In catch, prints "Error caught: " followed by the error.
+// In finally, prints "Check finished.".
+// Test your function with:
+// getData(5) → should print "Valid number: 5" and "Check finished.".
+// getData(-3) → should print "Error caught: Number must be positive!" and "Check finished.".
+
+
+
+
+// What are Modules?
+
+// Modules are separate files of code that can be imported and reused in other files.
+
+// Helps organize code better.
+// Avoids repeating code.
+// Makes large projects easier to manage.
+
+// Exporting: Share code (functions, variables, classes) from a module.
+// Importing: Use code from another module. 
