321c04c3880162758d4d334e7a0065aa36b077aa
Madhav Poudel <madhavpoudel78@nepathyacollege.edu.np>
2025-12-03 13:53:51 +0545

    GET Method in ExpressJS

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -188,25 +188,60 @@
 
 
 
+// const express = require("express");
+// const app = express();
+// const port = 5000;
+
+// // To read JSON body (Postman raw -> JSON)
+// app.use(express.json());
+
+// // To read form data (Postman x-www-form-urlencoded)
+// app.use(express.urlencoded({ extended: true }));
+
+// // POST Route
+// app.post("/contact", (req, res) => {
+//   const { name, email } = req.body;
+  
+//   res.send({
+//     message: "Data received successfully!!",
+//     name: name,
+//     email: email
+//   });
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+
+
+// GET Request with Query Params and URL Params
 const express = require("express");
 const app = express();
 const port = 5000;
 
-// To read JSON body (Postman raw -> JSON)
-app.use(express.json());
+// Simple GET
+app.get("/info", (req, res) => {
+  res.send("Server is running.");
+});
 
-// To read form data (Postman x-www-form-urlencoded)
-app.use(express.urlencoded({ extended: true }));
+// GET with Query Params
+app.get("/user", (req, res) => {
+  const { name, age } = req.query;
 
-// POST Route
-app.post("/contact", (req, res) => {
-  const { name, email } = req.body;
-  
-  res.send({
-    message: "Data received successfully",
-    name: name,
-    email: email
-  });
+  res.send(
+    `Query Received\nName: ${name}\nAge: ${age}`
+  );
+});
+
+// GET with URL Params
+app.get("/product/:id", (req, res) => {
+  const productId = req.params.id;
+
+  res.send(
+    `Product ID Received: ${productId}`
+  );
 });
 
 // Start server
